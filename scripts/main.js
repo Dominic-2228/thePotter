69e1630e7b2adba7610d85c491e4bbc82ae7cb6a
Dominic Tillison <22tilldj@gmail.com>
2025-03-17 14:57:09 -0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import { makePottery } from "./PotteryWheel.js";
+
+const potter = makePottery("circle", 3, 3)
+const potter2 = makePottery("mud", 3, 3)
+const potter3 = makePottery("round", 3, 3)
+const potter4 = makePottery("triangle", 3, 3)
+const potter5 = makePottery("hour glass", 3, 3)
+const potter6 = makePottery("platter", 3, 3)
+console.log(potter)
